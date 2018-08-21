333d5ec72542d200ad35f7b52e272f531e73ecd4
Fredrik Carlsson <FreddeFc@gmail.com>
2018-08-21 10:59:06 +0200

    Additional UI Fixes

---
+++ Assets/Connect4WinFlicker.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Connect4 {
+
+    public class Connect4WinFlicker : MonoBehaviour {
+
+        public MeshRenderer theMesh;
+        private float minOpacity = 0.2f, maxOpacity = 1, fadeSpeed = 1;
+        private float currentOpacity = 0.2f;
+        private bool fadingUp = true;
+
+        // Use this for initialization
+        void Start() {
+            setOpacity(minOpacity);
+            StartCoroutine(fadeColor());
+        }
+
+        private IEnumerator fadeColor() {
+            while (true) {
+                yield return new WaitForEndOfFrame();
+
+                if (fadingUp) {
+                    currentOpacity += Time.deltaTime * fadeSpeed;
+                    if (currentOpacity >= maxOpacity)
+                        fadingUp = false;
+                    currentOpacity = Mathf.Clamp(currentOpacity, minOpacity, maxOpacity);
+                }
+                else {
+                    currentOpacity -= Time.deltaTime * fadeSpeed;
+                    if (currentOpacity <= minOpacity)
+                        fadingUp = true;
+                    currentOpacity = Mathf.Clamp(currentOpacity, minOpacity, maxOpacity);
+
+                }
+
+                setOpacity(currentOpacity);
+            }
+        }
+
+        private void setOpacity(float o) {
+            Color c = theMesh.material.color;
+            c.a = o;
+            theMesh.material.color = c;
+        }
+    }
+}
