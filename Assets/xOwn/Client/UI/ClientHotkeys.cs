d1c8da6701e1fc441a01d871aa6a50e7e89f22eb
Fredrik Carlsson <FreddeFc@gmail.com>
2018-09-17 07:49:45 +0200

    Added Hotkeys For exit fullscreen

---
+++ Assets/xOwn/Client/UI/ClientHotkeys.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientHotkeys : MonoBehaviour {
+
+    public KeyCode exitFullScreenButton = KeyCode.Escape;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(exitFullScreenButton))
+            exitFullScreen();
+	}
+
+    private void exitFullScreen() {
+        Screen.fullScreen = false;
+    }
+}
