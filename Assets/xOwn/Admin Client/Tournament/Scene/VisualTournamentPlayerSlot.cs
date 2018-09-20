1db367feaa1356bdb7ea476c001c8a79c05b53d2
LinusBF <linus.webdevelopment@gmail.com>
2018-09-20 18:26:30 +0200

    Some work in progress. Removed extra header from tournament, Added BETA to header, added some colors to tournament playerslots

--- Assets/xOwn/Admin Client/Tournament/Scene/VisualTournamentPlayerSlot.cs
+++ Assets/xOwn/Admin Client/Tournament/Scene/VisualTournamentPlayerSlot.cs
@@ -14,15 +14,20 @@ namespace Tournament.Client {
         [SerializeField]
         private TextMeshPro text;
 
-        private static Color mutedPurBlue = new Color((36f/255f), (35f/255f), (74f/255f), 1f);
+        private static Color darkPurple = new Color((17f / 255f), (16f / 255f), (50f / 255f), 1f);
+        private static Color paleOrange = new Color((255f / 255f), (146f / 255f), (53f / 255f), 1f);
+        private static Color mutedPurBlue = new Color((36f / 255f), (35f / 255f), (74f / 255f), 1f);
         private static Color pureBlue = new Color((60f / 255f), (53f / 255f), (254f / 255f), 1f);
-        private Color emptyColor = mutedPurBlue;
+        private static Color lightBlue = new Color((82f / 255f), (231f / 255f), (254f / 255f), 1f);
+        private Color emptyColor = darkPurple;
         private Color idleColor = mutedPurBlue;
-        private Color lobbyNotReadyColor = Color.yellow;
-        private Color lobbyReadyColor = Color.green;
-        private Color playingColor = Color.green;
-        private Color overLostColor = mutedPurBlue;
+        private Color lobbyNotReadyColor = paleOrange;
+        private Color lobbyReadyColor = lightBlue;
+        private Color playingColor = pureBlue;
+        private Color overLostColor = darkPurple;
         private Color overWonColor = pureBlue;
+        private Color lightTextColor = Color.white;
+        private Color darkTextColor = Color.black;
 
         private void Awake() {
             setBackgroundColor(null, RoundState.Empty);
@@ -35,30 +40,31 @@ namespace Tournament.Client {
 
         private void setBackgroundColor(TournamentPlayer player, RoundState state) {
             switch (state) {
-                case RoundState.Empty: setColor(emptyColor); break;
-                case RoundState.Idle: setColor(idleColor); break;
-                case RoundState.Playing: setColor(emptyColor); break;
+                case RoundState.Empty: setColor(emptyColor, false); break;
+                case RoundState.Idle: setColor(idleColor, false); break;
+                case RoundState.Playing: setColor(emptyColor, false); break;
                 case RoundState.Over:
                 if (player.isWinning) {
-                    setColor(overWonColor);
+                    setColor(overWonColor, false);
                 } else {
-                    setColor(overLostColor);
+                    setColor(overLostColor, false);
                 }
                 break;
 
                 case RoundState.Lobby:
                 if (player != null && player.isReady)
-                    setColor(lobbyReadyColor);
+                    setColor(lobbyReadyColor, true);
                 else
-                    setColor(lobbyNotReadyColor);
+                    setColor(lobbyNotReadyColor, true);
                 break;
             }
         }
 
+        private void setColor(Color newColor, bool darkText) {
+            theRenderer.material.color = newColor;
+            text.color = (darkText ? darkTextColor: lightTextColor);
+        }
 
-
-        private void setColor(Color newColor) {theRenderer.material.color = newColor;}
-        
         /*
         public void setState(RoundState state) {
             this.state = state;
