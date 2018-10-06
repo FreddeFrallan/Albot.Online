d359c4e47f12468d158fcf2b4b1c95ce8c41d990
FreddeFrallan <FreddeFc@gmail.com>
2018-10-06 22:49:21 +0200

    Revert "LoginData ala Fredde"
    This reverts commit cf61f52ae24e10fc19d04215712000a9047682fd.

---
+++ Assets/xOwn/Admin Client/Stats/scripts/LoginData.cs
@@ -0,0 +1,28 @@
+﻿using ChartAndGraph;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginData : MonoBehaviour {
+
+    public GraphChart chart;
+
+    private string DataCategory = "Logins";
+
+    // Use this for initialization
+    void Start () {
+        chart.DataSource.StartBatch();
+
+        chart.DataSource.ClearCategory(DataCategory);
+        
+        for(int i = 1; i <= 24; i++)
+            chart.DataSource.AddPointToCategory(DataCategory, i, i*i);
+
+        chart.DataSource.EndBatch();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
